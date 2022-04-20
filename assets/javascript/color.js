3554a7b7fd1ba97eb5d8c12bea0e255c2998e927
fonko007 <snowfoxmc123@gmail.com>
2022-04-20 09:37:05 +0700

    updated

--- assets/javascript/color.js
+++
@@ -1,5 +0,0 @@
-var color = document.getElementById("color");
-
-color.addEventListener("input", function(){
-	document.querySelector(".result").innerHTML = parseInt(color.value.slice(1), 16);
-}, false);
